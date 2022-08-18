ca6377b739bab4c6c2d0fe2ac1bfd05663fba73a
Cicera Bruna <cicerabruna@gmail.com>
2022-08-18 11:27:44 -0300

    aula 6

---
+++ aula6.py
@@ -0,0 +1,18 @@
+from ftplib import parse150
+
+
+for i in range(10):
+    print(1) 
+lista_nomes = ['André','Felipe']
+for nome in lista_nomes:
+    print(nome)
+for numero in range(9,-1,-1):
+    print(numero)
+for letra in 'Andre Felipe':
+    print(letra)
+    # for 
+    
+
+    
+dadoSensorInercial = [0.988,3.52]
+# dadosSensorE
